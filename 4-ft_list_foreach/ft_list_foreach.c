d1b54543cfb86fe1c9d4cfe63f81f9964d9a7ba7
Anastasia Lukyanenko <vicooodin@gmail.com>
2018-03-19 16:32:47 +0200

    Add files via upload

---
+++ 4-ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alukyane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/23 21:30:52 by alukyane          #+#    #+#             */
+/*   Updated: 2017/10/23 21:30:54 by alukyane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	while (begin_list)
+	{
+		(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
